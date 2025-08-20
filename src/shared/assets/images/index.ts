f227e41a3a531eb8b9394e4e36595632420ec2e0
JeongEuiwang <98511234+JeongEuiWang@users.noreply.github.com>
2025-08-21 05:19:09 +0900

    ...

--- src/shared/assets/images/index.ts
+++ src/shared/assets/images/index.ts
@@ -1,2 +1,2 @@
-export { default as SignBackground } from "./img/img_sign_in.png";
+export { default as SignBackground } from "./img/img_sign_in.jpg";
 export { default as KaiaIcon } from "./icon/icon_kaia_dark.svg";
